ba646e1bf960014f6383514ee469f0ed1206e08f
JMB <137451269+Wade-Possible@users.noreply.github.com>
2024-03-01 16:27:33 +0100

    Redo

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,17 @@ const playerPoints = document.getElementById("player-points");
 const cpuPoints = document.getElementById("cpu-points");
 const playerImage = document.getElementById("player-image");
 const cpuImage = document.getElementById("cpu-image");
-const message = document.getElementById("message");
+const gameResult = document.getElementById("messages");
 const choices = ["rock", "paper", "scissors"];
+
+let playerScore = 0;
+let cpuScore = 0;
 
 // Setting up event listeners
 for (let button of buttons) {
     button.addEventListener("click", function () {
         let playerChoice = this.getAttribute("data-choice");
-        playGame("playerChoice");
+        playGame(playerChoice);
     });
 }
 
-// 
-function playGame(playerChoice) {
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
-
-    let cpuChoice = Math.floor(Math.random() * choices.length);
-
-    cpuImage.src = `assets/images/${choices[cpuChoice]}.png`;
-    cpuImage.alt = choices[cpuChoice];
-
-let result = checkWinner(choices[playerChoice], choices[cpuChoice]);
-
-updatePoints(result);
-}
-
-playGame(playerChoice);
